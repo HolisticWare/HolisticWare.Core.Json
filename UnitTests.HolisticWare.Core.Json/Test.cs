a7973ebb0a951cadef42c85e31e16d53144d4675
moljac <mcvjetko@holisticware.net>
2015-02-12 23:30:44 +0100

    nuget packages created

---
+++ UnitTests.HolisticWare.Core.Json/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UniTests.HolisticWare.Core.Json
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
